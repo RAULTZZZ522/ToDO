b82e832636cbcb1298676e3eac762d29d8b5bb2c
Chen <robertssvq@gmail.com>
2025-06-27 10:59:30 +0800

    完成云开发初始化

--- app.js
+++ app.js
@@ -1,66 +1,21 @@
 // app.js
-import config from './config';
-import Mock from './mock/index';
-import createBus from './utils/eventBus';
-import { connectSocket, fetchUnreadNum } from './mock/chat';
-
-if (config.isMock) {
-  Mock();
-}
-
 App({
-  onLaunch() {
-    const updateManager = wx.getUpdateManager();
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        // env 参数说明：
+        // env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+        // 此处请填入环境 ID, 环境 ID 可打开云控制台查看
+        // 如不填则使用默认环境（第一个创建的环境）
+        env: 'cloud-dev-1-xxxx',
+        traceUser: true,
+      })
+    }
 
-    updateManager.onCheckForUpdate((res) => {
-      // console.log(res.hasUpdate)
-    });
-
-    updateManager.onUpdateReady(() => {
-      wx.showModal({
-        title: '更新提示',
-        content: '新版本已经准备好，是否重启应用？',
-        success(res) {
-          if (res.confirm) {
-            updateManager.applyUpdate();
-          }
-        },
-      });
-    });
-
-    this.getUnreadNum();
-    this.connect();
-  },
-  globalData: {
-    userInfo: null,
-    unreadNum: 0, // 未读消息数量
-    socket: null, // SocketTask 对象
-  },
-
-  /** 全局事件总线 */
-  eventBus: createBus(),
-
-  /** 初始化WebSocket */
-  connect() {
-    const socket = connectSocket();
-    socket.onMessage((data) => {
-      data = JSON.parse(data);
-      if (data.type === 'message' && !data.data.message.read) this.setUnreadNum(this.globalData.unreadNum + 1);
-    });
-    this.globalData.socket = socket;
-  },
-
-  /** 获取未读消息数量 */
-  getUnreadNum() {
-    fetchUnreadNum().then(({ data }) => {
-      this.globalData.unreadNum = data;
-      this.eventBus.emit('unread-num-change', data);
-    });
-  },
-
-  /** 设置未读消息数量 */
-  setUnreadNum(unreadNum) {
-    this.globalData.unreadNum = unreadNum;
-    this.eventBus.emit('unread-num-change', unreadNum);
-  },
-});
+    this.globalData = {
+      userInfo: null
+    }
+  }
+})
---
+++ cloudfunctions/README.md
@@ -0,0 +1,8 @@
+# 云函数目录
+
+这是云开发的云函数目录，云函数是一段运行在云端的代码，无需管理服务器，在小程序中调用即可运行。
+
+## 参考文档
+
+- [云函数介绍](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/basis/capabilities.html#云函数)
+- [云函数示例代码](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/guide/functions/getting-started.html) 
