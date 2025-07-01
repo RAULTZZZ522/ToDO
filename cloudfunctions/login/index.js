12ae2d2fe3a21fa3d3e5c12d7dba299fad0be2eb
Chen <robertssvq@gmail.com>
2025-07-01 11:14:04 +0800

    登录功能初始化

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -10,21 +10,22 @@ cloud.init({
 })
 
 /**
- * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端，
- * 获取用户信息需要 wx.getUserInfo，需要额外鉴权
- * 
+ * 云函数：用户登录
+ * 功能：获取用户openid并返回
  */
 exports.main = async (event, context) => {
-  console.log(event)
-  console.log(context)
-
-  // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）等信息
+  // 获取微信上下文
   const wxContext = cloud.getWXContext()
+  const openid = wxContext.OPENID
+  const appid = wxContext.APPID
+  const unionid = wxContext.UNIONID || ''
 
+  // 返回数据
   return {
     event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+    openid,
+    appid,
+    unionid,
+    success: true
   }
 } 
--- miniprogram/pages/userCenter/index.js
+++ miniprogram/pages/userCenter/index.js
@@ -1,8 +1,109 @@
 Page({
   data: {
-    userInfo: {
-      avatar: '../../images/icons/usercenter.png',
-      nickname: '微信用户'
+    userInfo: null,
+    hasUserInfo: false
+  },
+
+  onLoad: function () {
+    // 检查是否已有登录信息
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.setData({
+        userInfo: userInfo,
+        hasUserInfo: true
+      });
     }
+  },
+
+  // 获取用户信息
+  onGetUserInfo: function (e) {
+    if (e.detail.userInfo) {
+      const userInfo = e.detail.userInfo;
+
+      // 调用微信登录获取openid
+      wx.login({
+        success: (loginRes) => {
+          // 获取code，用于后续获取openid
+          const code = loginRes.code;
+
+          // 调用云函数登录，获取openid
+          wx.cloud.callFunction({
+            name: 'login',
+            data: { code },
+            success: res => {
+              // 合并用户信息
+              const userData = {
+                ...userInfo,
+                openid: res.result.openid
+              };
+
+              // 保存到本地
+              wx.setStorageSync('userInfo', userData);
+
+              this.setData({
+                userInfo: userData,
+                hasUserInfo: true
+              });
+
+              wx.showToast({
+                title: '登录成功',
+                icon: 'success'
+              });
+            },
+            fail: err => {
+              console.error('获取用户openid失败', err);
+              // 即使没有openid，也可以显示基本用户信息
+              wx.setStorageSync('userInfo', userInfo);
+              this.setData({
+                userInfo: userInfo,
+                hasUserInfo: true
+              });
+            }
+          });
+        }
+      });
+    } else {
+      wx.showToast({
+        title: '您拒绝了授权',
+        icon: 'none'
+      });
+    }
+  },
+
+  logout: function () {
+    wx.showModal({
+      title: '提示',
+      content: '确定要退出登录吗？',
+      success: (res) => {
+        if (res.confirm) {
+          // 清除本地存储的用户信息
+          wx.removeStorageSync('userInfo');
+
+          this.setData({
+            userInfo: null,
+            hasUserInfo: false
+          });
+
+          wx.showToast({
+            title: '已退出登录',
+            icon: 'success'
+          });
+        }
+      }
+    });
+  },
+
+  goToSettings: function () {
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
+  },
+
+  goToAbout: function () {
+    wx.showToast({
+      title: '关于应用',
+      icon: 'none'
+    });
   }
 }) 
